9de5bb2186850f672a25b0661847381c197e0d6a
Wansen <35102939+VincentSong524@users.noreply.github.com>
2025-02-06 19:06:30 +0800

    常数阶、线性阶

---
+++ codes/python/practice/2_3_1.py
@@ -0,0 +1,13 @@
+# 算法 A 的时间复杂度：常数阶
+def algorithm_A(n: int):
+    print(0)
+
+# 算法 B 的时间复杂度：线性阶
+def algorithm_B(n: int):
+    for _ in range(n):
+        print(0)
+
+# 算法 C 的时间复杂度：常数阶
+def algorithm_C(n: int):
+    for _ in range(1000000):
+        print(0)
